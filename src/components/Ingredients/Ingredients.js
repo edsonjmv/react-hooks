8a7143252cf0e19f6f181339c23d3536a4b198dd
Edson Machado <edsonjmv@gmail.com>
2021-06-24 12:34:21 +0200

    useEffect & loading data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,6 +6,23 @@ import Search from './Search';
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      'https://react-hooks-889a0-default-rtdb.firebaseio.com/ingredients.json'
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            ...responseData[key],
+            id: key
+          });
+        }
+        setIngredients(loadedIngredients);
+      });
+  }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch(
